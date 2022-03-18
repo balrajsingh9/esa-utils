66bb7d8152fe11e4f9d0acab73d7871e293e2d20
Balraj Singh <37571874+balrajsingh9@users.noreply.github.com>
2022-03-18 11:26:32 +0100

    feat: impl union using linked list

--- core/maximal_repeats.py
+++ core/maximal_repeats.py
@@ -1,6 +1,29 @@
 from __future__ import annotations
 
 
+class Node:
+    def __init__(self, data):
+        self.__data = data
+        self.__next = None
+
+    def get_data(self):
+        return self.__data
+
+    def get_next(self):
+        return self.__next
+
+    def set_data(self, data):
+        self.__data = data
+
+    def set_next(self, nextNode):
+        self.__next = nextNode
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
 class Interval:
     def __init__(self, lcp_value: int = 0, lb: int = 0, rb: int = -1, child_list=None):
         if child_list is None:
@@ -9,6 +32,7 @@ class Interval:
         self.__lb: int = lb
         self.__rb: int = rb
         self.__child_list = child_list
+        self.__pos_sets = None
 
     def get_lcp_value(self):
         return self.__lcp_value
@@ -22,6 +46,9 @@ class Interval:
     def get_child_list(self):
         return self.__child_list
 
+    def get_pos_sets(self):
+        return self.__pos_sets
+
     def update_child_list(self, interval):
         self.__child_list.append(interval)
 
@@ -33,6 +60,12 @@ class Interval:
 
     def set_lcp_value(self, lcp_value: int):
         self.__lcp_value = lcp_value
+
+    def set_pos_sets(self, pos_sets):
+        self.__pos_sets = pos_sets
+
+    def __str__(self):
+        return f"lb=%s, rb=%s, lcp_value=%s" % (self.__lb, self.__rb, self.__lcp_value)
 
 
 def add(top_interval: Interval, last_interval: Interval):
@@ -65,8 +98,55 @@ class SpecialStack:
         return self.__len == 0
 
 
-def process(lcp_interval: Interval):
-    pass
+def process(lcp_interval: Interval, suf_tab, s):
+    pos_set_list = LinkedList()
+    prev = None
+
+    pos_set = []
+    l = lcp_interval.get_lcp_value()
+
+    for j in range(0, len(lcp_interval.get_child_list())):
+        pos_set = [-1] * 26
+        child_interval: Interval = lcp_interval.get_child_list()[j]
+        p_set = set()
+        lb = child_interval.get_lb()
+        rb = child_interval.get_rb()
+
+        for i in range(lb, rb + 1):
+            p_set.add(suf_tab[i])
+
+        for i in range(0, 26):
+            for p in p_set:
+                if p != 0 and (s[p - 1] == chr(i + 97)):
+                    pos_set[i] = p
+
+        if pos_set_list.head is None:
+            pos_set_list.head = Node(pos_set)
+            prev = pos_set_list.head
+        else:
+            new_node = Node(pos_set)
+            new_node.set_next(prev.get_next())
+            prev.set_next(new_node)
+            prev = new_node
+
+        curr = pos_set_list.head
+
+        while curr is not None:
+            last_pos_set = curr.get_data()
+
+            for i in range(0, 26):
+                for k in range(0, 26):
+                    p = last_pos_set[i]
+                    if i != k and last_pos_set[i] != -1 and pos_set[k] != -1:
+                        p_prime = pos_set[k]
+
+                        if p < p_prime:
+                            print(f"(%s, %s), (%s, %s)" % (
+                                p, p + l - 1, p_prime, p_prime + l - 1))
+
+            curr = curr.get_next()
+
+    lcp_interval.set_pos_sets(pos_set_list)
 
 
 def perform_bottom_up_traversal(s, suftab, lcp_table):
@@ -83,7 +163,7 @@ def perform_bottom_up_traversal(s, suftab, lcp_table):
             top.set_rb(i - 1)
             last_interval: Interval = stack.pop()
 
-            process(last_interval)
+            process(last_interval, suftab, s)
 
             lb: int = last_interval.get_lb()
             top = stack.top()
@@ -99,3 +179,5 @@ def perform_bottom_up_traversal(s, suftab, lcp_table):
             else:
                 stack.push(Interval(lcp_table[i], lb, -1, []))
 
+    stack.top().set_rb(len(lcp_table) - 1)
+    process(stack.pop(), suftab, s)
