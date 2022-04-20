c9959c823bcaae1d5d50382b085b393f2c1f0e9a
Balraj Singh <37571874+balrajsingh9@users.noreply.github.com>
2022-04-20 02:17:14 +0200

    use bwt table instead of looking up in s and refactor heavily

--- core/maximal_repeats.py
+++ core/maximal_repeats.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
-from typing import Optional
+from typing import Optional, Set, Tuple
 
-
-repeats_set: set[tuple[str, str]] = set()
+repeats_set: set[tuple[tuple[int, int], tuple[int, int]]] = set()
 
 
 @dataclass
@@ -48,30 +47,27 @@ class SpecialStack:
         return len(self.__stack) == 0
 
 
-def process(lcp_interval: Interval, suf_tab: list[int], s: str) -> None:
+def process(lcp_interval: Interval, suf_tab: list[int], s: str, bwt_table: list[str]) -> None:
     # linked list to union the position sets and save it to the parent node(propagate)
     pos_set_list_head: Optional[Node] = None
     prev: Optional[Node] = None
     # init the alphabet set over the input string
     alphabet_set: set[str] = set(s)
     # init the dict that maps the alphabet to a pos p, -1 is for undefined at init
-    alphabet_pos_map: dict[str, int] = {alphabet: -1 for alphabet in alphabet_set}
-    lcp: int = lcp_interval.lcp_value
+    alphabet_pos_map: dict[str, int] = {alphabet: 0 for alphabet in alphabet_set}
+    lcp = lcp_interval.lcp_value
 
     for j in range(0, len(lcp_interval.child_list)):
         child_interval: Interval = lcp_interval.child_list[j]
-        # position set for this child
-        p_set: set[int] = set()
         lb: int = child_interval.lb
         rb: int = child_interval.rb
 
-        for i in range(lb, rb + 1):
-            p_set.add(suf_tab[i])
-
         for alphabet in alphabet_set:
-            for p in p_set:
-                if p != 0 and (s[p - 1] == alphabet):
-                    alphabet_pos_map[alphabet] = p
+            for i in range(lb, rb + 1):
+                if bwt_table[i] == alphabet:
+                    alphabet_pos_map[alphabet] = suf_tab[i]
+
+        print(alphabet_pos_map)
 
         # if this is the first child in this subtree
         if pos_set_list_head is None:
@@ -92,18 +88,12 @@ def process(lcp_interval: Interval, suf_tab: list[int], s: str) -> None:
 
             for a in alphabet_set:
                 for b in alphabet_set:
-                    p = last_alphabet_pos_map[a]
-
-                    if a != b and last_alphabet_pos_map[a] != -1 and alphabet_pos_map[b] != -1:
+                    if a != b:
+                        p = last_alphabet_pos_map[a]
                         p_prime = alphabet_pos_map[b]
 
-                        # print(f"({p}, {p + lcp}), ({p_prime}, {p_prime + lcp})")
-                        left: str = s[p: p + lcp + 1]
-                        right: str = s[p_prime: p_prime + lcp + 1]
-
-                        # without this, I see abnormal pairs...don't know if this is ok to do?
-                        if left == right:
-                            repeats_set.add((left, right))
+                        if p < p_prime and bwt_table[p] != bwt_table[p_prime]:
+                            repeats_set.add(((p, p + lcp - 1), (p_prime, p_prime + lcp - 1)))
 
             curr = curr.next
 
@@ -111,7 +101,8 @@ def process(lcp_interval: Interval, suf_tab: list[int], s: str) -> None:
     lcp_interval.pos_sets = pos_set_list_head
 
 
-def perform_bottom_up_traversal(s, suf_tab, lcp_table) -> set[tuple[str, str]]:
+def perform_bottom_up_traversal(s: str, suf_tab: list[int], lcp_table: list[int], bwt_table: list[str]) -> \
+        set[tuple[tuple[int, int], tuple[int, int]]]:
     # add the sentinel
     s = s + '$'
     last_interval: Optional[Interval] = None
@@ -129,7 +120,7 @@ def perform_bottom_up_traversal(s, suf_tab, lcp_table) -> set[tuple[str, str]]:
             last_interval: Interval = stack.pop()
 
             # we know the child intervals now, process this subtree
-            process(last_interval, suf_tab, s)
+            process(last_interval, suf_tab, s, bwt_table)
 
             lb: int = last_interval.lb
             top: Interval = stack.top()
@@ -149,6 +140,6 @@ def perform_bottom_up_traversal(s, suf_tab, lcp_table) -> set[tuple[str, str]]:
     stack.top().rb = len(lcp_table)
 
     # process the root
-    process(stack.pop(), suf_tab, s)
+    process(stack.pop(), suf_tab, s, bwt_table)
 
     return repeats_set
