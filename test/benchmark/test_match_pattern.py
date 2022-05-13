f55949cc193a0341a3715b296b9124be2f6470b1
Balraj Singh <37571874+balrajsingh9@users.noreply.github.com>
2022-05-13 17:18:37 +0200

    test: add basic unit test and benchmark test

--- test/benchmark/test_match_pattern.py
+++ test/benchmark/test_match_pattern.py
@@ -1,24 +1,47 @@
 import time
 import unittest
+from typing import Optional
 
 from core.match_pattern import find_pattern
+from core.utils import basic_tables_utils
+from core.utils.STree import STree
 
 
 class TestMatchPattern(unittest.TestCase):
+    s: Optional[str] = None
+    p: Optional[str] = None
+    suf_tab: Optional[list[int]] = None
+    stree: Optional[STree] = None
+    lcp_tab: Optional[list[int]] = None
+    time_taken_using_esa = 0
+    time_taken_using_stree = 0
+
     @classmethod
-    def setUp(cls) -> None:
-        cls.s: str = "acaaacatat"
-        cls.suf_tab: list[int] = [2, 3, 0, 4, 6, 8, 1, 5, 7, 9, 10]
-        cls.lcp_table: list[int] = [0, 2, 1, 3, 1, 2, 0, 2, 0, 1, 0]
+    def setUpClass(cls) -> None:
+        cls.s = "banana"
+        cls.p = "ana"
+        cls.stree = STree()
+        cls.stree.build_using_mccreight(cls.s)
+        cls.suf_tab = basic_tables_utils.gen_suffix_array(cls.stree)
+        cls.lcp_tab = basic_tables_utils.gen_lcp_array(cls.s, cls.suf_tab)
 
-    def test_time_to_execute_find_pattern_without_slice(self):
+    @classmethod
+    def tearDownClass(cls) -> None:
+        print('\n\n--------- Benchmark Results Starts ---------\n')
+        print(f"Time taken to search for pattern using esa: {TestMatchPattern.time_taken_using_esa:.6f}")
+        print(f"Time taken to search for pattern using suffix tree: {TestMatchPattern.time_taken_using_stree:.6f}")
+        print('\n--------- Benchmark Result Ends ---------')
+
+    def test_time_to_search_pattern_using_esa(self):
         start_time = time.time()
-        find_pattern(self.s, "aca", self.suf_tab, self.lcp_table)
+        find_pattern(self.s, self.p, self.suf_tab, self.lcp_tab)
+        TestMatchPattern.time_taken_using_esa = time.time() - start_time
 
-        print(f"execution time in s={(time.time() - start_time):.6f}")
-
-    def test_time_to_execute_find_pattern_with_slice(self):
+    def test_time_to_search_pattern_using_stree(self):
         start_time = time.time()
-        find_pattern(self.s, "aca", self.suf_tab, self.lcp_table)
+        self.stree.search(self.p)
+        TestMatchPattern.time_taken_using_stree = time.time() - start_time
 
-        print(f"execution time in s={(time.time() - start_time):.6f}")
+
+if __name__ == '__main__':
+    unittest.main()
--- test/test_match_pattern.py
+++ test/test_match_pattern.py
@@ -1,17 +1,46 @@
 import unittest
+from typing import Optional
 
 from core.match_pattern import find_pattern
+from core.utils.STree import STree
+from core.utils import basic_tables_utils
 
 
 class TestMatchPattern(unittest.TestCase):
-    @classmethod
-    def setUp(cls) -> None:
-        cls.s: str = "acaaacatat"
-        cls.suf_tab: list[int] = [2, 3, 0, 4, 6, 8, 1, 5, 7, 9, 10]
-        cls.lcp_table: list[int] = [0, 2, 1, 3, 1, 2, 0, 2, 0, 1, 0]
+    s: Optional[str] = None
+    p: Optional[str] = None
+    non_existing_pattern: Optional[str] = None
+    suf_tab: Optional[list[int]] = None
+    stree: Optional[STree] = None
+    lcp_tab: Optional[list[int]] = None
 
-    def test_empty_pattern_in_non_empty_string(self) -> None:
-        self.assertIsNotNone(find_pattern(self.s, "", self.suf_tab, self.lcp_table))
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.s = "banana"
+        cls.p = "bana"
+        cls.non_existing_pattern = "haha"
+        cls.stree = STree()
+        cls.stree.build_using_mccreight(cls.s)
+        cls.suf_tab = basic_tables_utils.gen_suffix_array(cls.stree)
+        cls.lcp_tab = basic_tables_utils.gen_lcp_array(cls.s, cls.suf_tab)
+
+    def test_empty_pattern_in_non_empty_string_using_esa(self) -> None:
+        self.assertIsNotNone(find_pattern(self.s, "", self.suf_tab, self.lcp_tab))
+
+    def test_empty_pattern_in_non_empty_string_using_stree(self) -> None:
+        self.assertTrue(self.stree.search(""))
+
+    def test_valid_pattern_using_esa(self):
+        self.assertIsNotNone(find_pattern(self.s, self.p, self.suf_tab, self.lcp_tab))
+
+    def test_valid_pattern_using_stree(self):
+        self.assertTrue(self.stree.search(self.p))
+
+    def test_invalid_pattern_using_esa(self):
+        self.assertIsNone(find_pattern(self.s, self.non_existing_pattern, self.suf_tab, self.lcp_tab))
+
+    def test_invalid_pattern_using_stree(self):
+        self.assertFalse(self.stree.search(self.non_existing_pattern))
 
 
 if __name__ == '__main__':
